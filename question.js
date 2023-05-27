0337b82fd813c38f03ddedd92276766858cc9312
shivakumar.b <88093956+Shivakumarbala@users.noreply.github.com>
2023-05-27 14:49:28 +0530

    Online Quiz Application

---
+++ question.js
@@ -0,0 +1,58 @@
+let questions=[
+    {
+        numb : 1,
+        question : "what does html stands for?",
+        answer : "Hyper text markup language",
+        options: [
+            "Hyper text multiple language",
+            "Hyper text preprocessor",
+            "Hyper text multi language",
+            "Hyper text markup language"
+           ]
+    },
+    {
+        numb : 2,
+        question : "what does css stands for?",
+        answer : "cascaded style sheet",
+        options: [
+            "cipher text principle",
+            "cascaded style sheet",
+            "caesar cipher text",
+            "cedeated text"
+           ]
+    },
+    {
+        numb : 3,
+        question : "what does xmlstands for?",
+        answer : "extensible markup language",
+        options: [
+            "extra multi program language",
+            "executable multi language",
+            "extensible markup language",
+            "examine multiple language"
+           ]
+    },
+    {
+        numb : 4,
+        question : "what does sql stands for?",
+        answer : "structured query language",
+        options: [
+            "statement question language",
+            "stylesheet query language",
+            "stylish question language",
+            "structured query language"
+           ]
+    },
+    {
+        numb : 5,
+        question : "what does php stands for?",
+        answer : "Hyper text preprocessor",
+        options: [
+            "hyper text language",
+            "Hyper text programming",
+            "Hyper text preprocessor",
+            "Hyper text preprogramming"
+           ]
+    },
+
+];
